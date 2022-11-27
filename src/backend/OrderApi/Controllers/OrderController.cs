3f003929e89734632db345c412f8b8e9b3743977
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-11-27 11:53:25 +0100

    add api controller

---
+++ src/backend/OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApi.Repository;
+
+namespace OrderApi.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class OrderController : Controller
+{
+    private readonly IRepository _repository;
+    public OrderController(IRepository repository)
+    {
+        _repository = repository;
+    }
+}
