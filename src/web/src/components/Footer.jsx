506f0d830dda3aeca3e87f7c6f45b0704abb9635
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-01-08 22:10:27 +0100

    add footer component

---
+++ src/web/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return <div>{/* <Link to={"github.com"}>source code</Link> */}</div>;
+}
+
+export default Footer;
