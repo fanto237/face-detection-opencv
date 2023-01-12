c8336d05df30dfb98558cbfbcf1886cbf93d2685
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-01-12 21:48:32 +0100

    implement confirmation component

--- src/web/src/components/Confirmation.jsx
+++ src/web/src/components/Confirmation.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Confirmation() {
-  return (<div><iframe src="https://giphy.com/embed/u4CY9BW4umAfu" width="480" height="242" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>);
+
+
+function Confirmation({ values }) {
+
+  const SubmitedValue = values;
+
+  return (<div className="flex flex-col items-center justify-center">
+    <div><p>Thanks for submitting your order, you will soon received an email on the Adresse: {values.email} </p></div>
+    <iframe src="https://giphy.com/embed/u4CY9BW4umAfu" className="w-[900px] h-[400px]" allowFullScreen></iframe>
+  </div>);
 }
 
 export default Confirmation;
