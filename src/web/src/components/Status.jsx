0f08fcc199795f1c759e4341c96dd5926ce03c2e
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-03 12:52:05 +0100

    improving status components

--- src/web/src/components/Status.jsx
+++ src/web/src/components/Status.jsx
@@ -3,17 +3,18 @@ import { BiSearch } from "react-icons/bi";
 
 function Status() {
   return (
-    <div className="w-full mx-auto">
+    <div className="flex flex-col items-center justify-center w-full">
       <h1 className="text-[#1bd4f1] text-center text-3xl md:text-5xl pt-10">
-        Track an order
+        Track your order
       </h1>
 
-      <div className="flex items-center justify-center w-full py-10">
-        <input type="text" placeholder="Enter your order number" className=" border-[white] border-2 rounded w-[30%] text-center py-1" />
-        <button className="mx-10 px-16 py-1 rounded bg-[#1bd4f1] flex  items-center"><BiSearch size={28} className="bg-[#1bd4f1]" /><span className="pl-1 text-xl uppercase bg-inherit">Track</span></button>
+      <div className="flex flex-col items-center justify-center w-full py-10 sm:flex-row ">
+        <input type="text" placeholder="Enter your order number" className=" border-[white] border-2 rounded  text-center py-1 w-[320px] sm:w-96" />
+        <button className=" mt-2 sm:mt-0 sm:ml-2 py-[2px] px-2 rounded bg-[#1bd4f1] flex  items-center"><BiSearch size={28} className="bg-[#1bd4f1]" /><span className="pl-1 uppercase text-2sm bg-inherit">Track</span></button>
       </div>
 
-    </div>)
+    </div>
+  )
 }
 
 export default Status;
