8121638ee880ac87fe13be8d3d88bfe051dcedd6
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-06 17:41:20 +0100

    add cors authorization for local testing

--- src/backend/OrderApi/Program.cs
+++ src/backend/OrderApi/Program.cs
@@ -1,17 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using OrderApi.Data;
+using OrderApi.Mapping;
+using OrderApi.Models;
 using OrderApi.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("orderDbConnectionString");
+var frontEndUrl = builder.Configuration.GetValue<string>("frontend_url");
 
 // Add services to the container.
 
-var connectionString = builder.Configuration.GetConnectionString("orderDbConnectionString");
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseNpgsql(connectionString) // configuring the context to use the postgres provider
+    // configuring the context to use the postgres provider
+    options => options.UseNpgsql(connectionString) 
 );
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSingleton<IMapper, Mapper>();
+
+// registering and configuring asp.net.cors services to allow api call from others url or port
+builder.Services.AddCors(op => op.AddPolicy("cors-policy", policyBuilder =>
+{
+    policyBuilder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+}));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,11 +33,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+// adding a middleware for using asp.net.cors
+app.UseCors("cors-policy");
 
 app.UseAuthorization();
 
