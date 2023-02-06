1e40d723edd97d2fd0b9f42cfc5386a67e6b7808
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-06 17:42:41 +0100

    update migrations

---
+++ src/backend/OrderApi/Migrations/20230206110051_UpdateOrderTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Orders",
+                newName: "ImageName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageName",
+                table: "Orders",
+                newName: "ImageUrl");
+        }
+    }
+}
