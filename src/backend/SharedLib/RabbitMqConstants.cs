e53e5d3359ab3944d4780e5ed93de42f016c1c65
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-07 22:40:17 +0100

    add rabbitmq constantes

---
+++ src/backend/SharedLib/RabbitMqConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedLib;
+
+public static class RabbitMqConstants
+{
+    public const string RmqUri = "localhost";
+    public const string RmqUsername = "guest";
+    public const string RmqPassword = "guest";
+    public const string OrderRegisteredEvent = "order.registered.event";
+    public const string OrderProcessedEvent = "order.processed.event";
+    public const string OrderSentEvent = "order.sent.event";
+}
