8aa398b62c43ca5660f8ff1b6b9690d1aca80f6d
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-07 22:42:20 +0100

    add masstransit service to di in faceapi

--- src/backend/FaceProcessApi/Program.cs
+++ src/backend/FaceProcessApi/Program.cs
@@ -1,3 +1,9 @@
+// using FaceProcessApi.Consumers;
+
+using FaceProcessApi.Consumers;
+using MassTransit;
+using SharedLib;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +13,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<OrderRegisterdConsumer>();
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("localhost", "/", cfgHost =>
+        {
+            cfgHost.Username(RabbitMqConstants.RmqUsername);
+            cfgHost.Password(RabbitMqConstants.RmqPassword);
+        });
+        
+        cfg.ReceiveEndpoint(RabbitMqConstants.OrderProcessedEvent, configEndpoint =>
+        {
+            configEndpoint.ConfigureConsumer<OrderRegisterdConsumer>(ctx);
+        } );
+    });
+});
+
+builder.Services.AddSingleton<IFaceHandler, FaceHandler>();
 
 var app = builder.Build();
 
