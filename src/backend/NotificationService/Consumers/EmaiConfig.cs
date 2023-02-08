9c390f21ffb1ddb54029c8d9f4984453b50a670e
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-08 23:11:14 +0100

    implement notification service as a background task

---
+++ src/backend/NotificationService/Consumers/EmaiConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace MailService.Consumers;
+
+public class EmaiConfig
+{
+    public string Name { get; set; }
+    public string From { get; set; }
+    public string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
