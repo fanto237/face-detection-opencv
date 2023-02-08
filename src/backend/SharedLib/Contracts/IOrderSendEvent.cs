a50bfb85c6747809eb259eeb2ae21071095e92a5
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-08 23:11:51 +0100

    add new event for odersend

---
+++ src/backend/SharedLib/Contracts/IOrderSendEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedLib.Contracts;
+
+public interface IOrderSendEvent
+{
+    public Guid OrderId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string ImageName { get; set; }
+    public List<byte[]> Faces { get; set; }
+}
