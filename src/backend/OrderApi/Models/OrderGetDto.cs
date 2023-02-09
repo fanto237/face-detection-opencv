8bd35e84671f05957635f1176852d193fdfdb561
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-09 23:04:00 +0100

    add dto for get request

---
+++ src/backend/OrderApi/Models/OrderGetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderApi.Models;
+
+public class OrderGetDto
+{
+    public Guid OrderId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public OrderStatus Status { get; set; }
+    public byte[] ImageData { get; set; }
+}
