15fc591b3bbfbc123ca45ae9b18659016bb87344
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-12 21:23:11 +0100

    update endpoint for docker api

--- src/web/src/api/endPoint.js
+++ src/web/src/api/endPoint.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "https://localhost:7259",
+  baseURL: "https://localhost:5000",
 });
 
 instance.defaults.headers.common["Authorization"] = "Auth from an instance"
