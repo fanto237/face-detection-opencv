ca1305b6dfaddb01d752c34dc9962368f60c4639
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-17 09:10:28 +0100

    improve about component

--- src/web/src/api/endPoint.js
+++ src/web/src/api/endPoint.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "https://localhost:5000",
+  baseURL: "http://45.137.68.35:5000",
 });
 
 instance.defaults.headers.common["Authorization"] = "Auth from an instance"
