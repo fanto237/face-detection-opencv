dc32c56372368e72deb0cb6780d90e12ee36725e
Lucien <58596314+fantoSama@users.noreply.github.com>
2023-02-25 22:08:24 +0100

    add dbcontext

---
+++ src/backend/OrderApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Models;
+
+namespace OrderApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+    {
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Face> Faces { get; set; }
+}
