463cabcb7da5c33f093ee33a787fc973a08fece7
Lucien <me@fanto.dev>
2024-12-30 13:02:20 +0100

    upgrade dependencies and version to .net 8

---
+++ src/backend/ComputerVisionService.Tests/Services/FaceDetectionServicesTests.cs
@@ -0,0 +1,35 @@
+﻿using ComputerVisionService.Services;
+
+namespace ComputerVisionService.Tests.Services;
+
+public class FaceDetectionServicesTests
+{
+    private readonly IFaceDetectionServices _faceDetectionServices = new FaceDetectionServices();
+
+    [Fact]
+    public async Task ExtractFaces_ShouldReturnFaceData()
+    {
+        // Arrange
+        var testImage = await File.ReadAllBytesAsync("Services/best-image.jpg");
+
+        // Act
+        var faces = await _faceDetectionServices.ExtractFaces(testImage);
+
+        // Assert
+        Assert.NotEmpty(faces);
+        Assert.All(faces, Assert.NotNull);
+    }
+
+    [Fact]
+    public async Task ExtractFaces_NoFacesDetected_ShouldReturnEmptyList()
+    {
+        // Arrange
+        var testImage = await File.ReadAllBytesAsync("Services/no-face.jpg");
+
+        // Act
+        var faces = await _faceDetectionServices.ExtractFaces(testImage);
+
+        // Assert
+        Assert.Empty(faces);
+    }
+}
