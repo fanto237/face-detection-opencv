04276bcf64fbe41a7bded8e557b7839c0465047b
Lucien <me@fanto.dev>
2024-12-30 13:09:00 +0100

    register auth in the DI containter

--- src/backend/OrderApi/Program.cs
+++ src/backend/OrderApi/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OrderApi.Consumers;
 using OrderApi.Data;
 using OrderApi.Mapping;
 using OrderApi.Repository;
+using OrderApi.Services;
 using SharedLib;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,14 +22,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql(connectionString)
 );
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IFaceRepository, FaceRepository>();
-builder.Services.AddSingleton<IMapper, Mapper>();
+builder.Services.AddScoped<IFileProcessingService, FileProcessingService>();
+builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+// configuring the authentication and authorization
+builder.Services.AddAuthentication(
+    options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(
+        options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer= builder.Configuration["Jwt:Issuer"],
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Secret"])),
+                ValidateIssuerSigningKey = true
+            };
+        }
+    );
 
 // registering and configuring asp.net.cors services to allow api call from others url or port
 builder.Services.AddCors(op => op.AddPolicy("cors-policy", policyBuilder =>
 {
-    policyBuilder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
-    // policyBuilder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+    // policyBuilder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    policyBuilder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
 }));
 
 builder.Services.AddMassTransit(config =>
@@ -49,7 +79,37 @@ builder.Services.AddMassTransit(config =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.  \r\n\r\n" +
+                          "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                          "Example: \"Bearer 123\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Scheme = "Bearer",
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    }
+);
 
 var app = builder.Build();
 
@@ -64,7 +124,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("cors-policy");
 
 // app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
