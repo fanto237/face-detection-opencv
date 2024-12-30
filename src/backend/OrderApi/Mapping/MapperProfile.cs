e96dde8228b8ede5eae8367912015fef6de7125e
Lucien <me@fanto.dev>
2024-12-30 13:05:41 +0100

    setup automapper

---
+++ src/backend/OrderApi/Mapping/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using OrderApi.Models;
+
+namespace OrderApi.Mapping;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<OrderCreateDto, Order>().ReverseMap();
+    }
+}
